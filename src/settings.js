9a4b46403a990f8982dc6e2f9b4ea318cf457ff0
victornwaiwu <victor.nwaiwu@andela.com>
2019-06-19 12:51:36 +0100

    add settings for api secret and client ids

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,7 @@
 require('dotenv').config({ silent: true });
 
 export default {
-  apiUrl: 'https://api.foursquare.com/v2/venues/explore',
+  apiUrl: 'https://api.foursquare.com/v2/search/recommendations',
+  clientId: process.env.REACT_APP_CLIENT_ID,
+  secretKey: process.env.REACT_APP_CLIENT_SECRET,
 };
